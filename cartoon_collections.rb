3817aafb6c1d10f3f7704803293c1ca889296b19
Valentino Wong <valentino.wong@gmail.com>
2019-07-02 18:00:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index do |n,i|
+    puts "#{i+1} #{n}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map do |n|
+    n.capitalize.concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? do |n|
+    n.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |n|
+    n == "cheddar" || n == "gouda" || n == "camembert"
+  end
 end
